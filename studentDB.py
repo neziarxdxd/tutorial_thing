3427eb9457d802bcaf07336b1f48cae8849bcfed
zenraiching <rickdavidsangalang@gmail.com>
2020-10-15 15:02:04 +0800

    add

--- studentDB.py
+++ studentDB.py
@@ -5,4 +5,9 @@ mySQL = connectSQL.connect(
     password= "",
     database = "db_school")
 myCursor= mySQL.cursor()
-myCursor.execute("SHOW TABLES")
+sql = '''INSERT INTO tbl_student (first_name, last_name,section) 
+        VALUES (%s, %s,%s)'''
+values = ("Juan","Dela Cruz","C301")
+myCursor.execute(sql, values)
+mySQL.commit()
+print(myCursor.rowcount)
