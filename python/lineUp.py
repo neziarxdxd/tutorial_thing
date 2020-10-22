2a42cbe6af27e978f08f6c38767d10827d8afa83
zenraiching <rickdavidsangalang@gmail.com>
2020-10-22 15:47:12 +0800

    passed test

--- python/lineUp.py
+++ python/lineUp.py
@@ -1,5 +1,26 @@
-'''
-aaabbbcaa
-3a3bc2a
+word= "aaabbbcaa"+"@"
+dictList = []
+count = 1
+for i in range(len(word)-1):
+    
+    
+    currentCount = []
 
-'''
+    
+
+    if word[i] == word[i+1]:
+        count+=1
+        print("passed",word[i],word[i+1],count)
+       
+
+    else:
+        print("else",count)
+        if count==1:
+            dictList.append([word[i]])
+        else:   
+            dictList.append([str(count),word[i]])
+        count=1
+        
+print("".join(["".join(dictList[i]) for i in range(len(dictList))]))
+        
+    
