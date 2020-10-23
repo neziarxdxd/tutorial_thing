e0344a2be07152edb231f20466e718ed51f34dad
zenraiching <rickdavidsangalang@gmail.com>
2020-10-23 09:31:10 +0800

    add

--- python/lineUp.py
+++ python/lineUp.py
@@ -1,17 +1,14 @@
 word= "aaabbbcaa"+"@"
 dictList = []
 count = 1
-for i in range(len(word)-1):
-    
-    
+for i in range(len(word)-1): 
     currentCount = []
-
     
 
     if word[i] == word[i+1]:
         count+=1
         print("passed",word[i],word[i+1],count)
-       
+ 
 
     else:
         print("else",count)
@@ -20,7 +17,6 @@ for i in range(len(word)-1):
         else:   
             dictList.append([str(count),word[i]])
         count=1
-        
 print("".join(["".join(dictList[i]) for i in range(len(dictList))]))
         
     
